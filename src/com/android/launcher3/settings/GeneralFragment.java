SlimRoms/packages_apps_SlimLauncher

SlimLauncher: Icon pack support
based on CMs Trebuchet

Change-Id: I2c3d29cfbceca060defd53980244e90dc46eb322

src/com/slim/slimlauncher/settings/GeneralFragment.java
@@ -1,7 +1,9 @@
 package com.slim.slimlauncher.settings;
 
 import android.os.Bundle;
+import android.preference.Preference;
 
+import com.slim.slimlauncher.IconPackHelper;
 import com.slim.slimlauncher.R;
 
 public class GeneralFragment extends SettingsPreferenceFragment {
@@ -11,5 +13,14 @@ public class GeneralFragment extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.general_preferences);
+
+        Preference iconPack = findPreference(SettingsProvider.KEY_ICON_PACK);
+        iconPack.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                IconPackHelper.pickIconPack(mContext, false);
+                return true;
+            }
+        });
     }
 }

Team-OctOS/platform_packages_apps_Launcher3

src/com/android/launcher3/settings/GeneralFragment.java
@@ -17,7 +17,9 @@
 package com.android.launcher3.settings;
 
 import android.os.Bundle;
+import android.preference.Preference;
 
+import com.android.launcher3.IconPackHelper;
 import com.android.launcher3.R;
 
 public class GeneralFragment extends SettingsPreferenceFragment {
@@ -27,5 +29,14 @@ public class GeneralFragment extends SettingsPreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.general_preferences);
+
+        Preference iconPack = findPreference(SettingsProvider.KEY_ICON_PACK);
+        iconPack.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                IconPackHelper.pickIconPack(mContext, false);
+                return true;
+            }
+        });
     }
 }