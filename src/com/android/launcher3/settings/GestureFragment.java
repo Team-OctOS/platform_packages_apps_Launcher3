SlimRoms/packages_apps_SlimLauncher

Add all gestures back
Change-Id: I49c54e84cf6e31e2b7124897d6e7333f592e169a

src/com/slim/slimlauncher/settings/GestureFragment.java
@@ -1,0 +1,139 @@
+package com.slim.slimlauncher.settings;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.view.MenuItem;
+
+import com.slim.slimlauncher.R;
+import com.slim.slimlauncher.util.AppHelper;
+import com.slim.slimlauncher.util.ShortcutPickHelper;
+
+import java.util.Set;
+
+public class GestureFragment extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    ShortcutPickHelper mPicker;
+
+    ListPreference mGestureLeftUp;
+    ListPreference mGestureMiddleUp;
+    ListPreference mGestureRightUp;
+    ListPreference mGestureLeftDown;
+    ListPreference mGestureMiddleDown;
+    ListPreference mGestureRightDown;
+    ListPreference mGesturePinch;
+    ListPreference mGestureSpread;
+    ListPreference mGestureDoubleTap;
+
+    String mGesture;
+    ListPreference mGestureCurrent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.gesture_fragment);
+
+        mGestureLeftUp = (ListPreference) findPreference(SettingsProvider.LEFT_UP_GESTURE_ACTION);
+        mGestureMiddleUp = (ListPreference)
+                findPreference(SettingsProvider.MIDDLE_UP_GESTURE_ACTION);
+        mGestureRightUp = (ListPreference) findPreference(SettingsProvider.RIGHT_UP_GESTURE_ACTION);
+        mGestureLeftDown = (ListPreference)
+                findPreference(SettingsProvider.LEFT_DOWN_GESTURE_ACTION);
+        mGestureMiddleDown = (ListPreference)
+                findPreference(SettingsProvider.MIDDLE_DOWN_GESTURE_ACTION);
+        mGestureRightDown = (ListPreference)
+                findPreference(SettingsProvider.RIGHT_DOWN_GESTURE_ACTION);
+        mGesturePinch = (ListPreference) findPreference(SettingsProvider.PINCH_GESTURE_ACTION);
+        mGestureSpread = (ListPreference) findPreference(SettingsProvider.SPREAD_GESTURE_ACTION);
+        mGestureDoubleTap = (ListPreference)
+                findPreference(SettingsProvider.DOUBLE_TAP_GESTURE_ACTION);
+
+        mGestureLeftUp.setOnPreferenceChangeListener(this);
+        mGestureMiddleUp.setOnPreferenceChangeListener(this);
+        mGestureRightUp.setOnPreferenceChangeListener(this);
+        mGestureLeftDown.setOnPreferenceChangeListener(this);
+        mGestureMiddleDown.setOnPreferenceChangeListener(this);
+        mGestureRightDown.setOnPreferenceChangeListener(this);
+        mGesturePinch.setOnPreferenceChangeListener(this);
+        mGestureSpread.setOnPreferenceChangeListener(this);
+        mGestureDoubleTap.setOnPreferenceChangeListener(this);
+
+        mPicker = new ShortcutPickHelper(getActivity(), new ShortcutPickHelper.OnPickListener() {
+            @Override
+            public void shortcutPicked(String uri, String friendlyName, boolean isApplication) {
+                if (uri == null) {
+                    return;
+                }
+                if (mGesture != null) {
+                    SettingsProvider.putString(mContext, mGesture + "_custom", uri);
+                    mGesture = null;
+                }
+                if (mGestureCurrent != null) {
+                    updateSummary(mGestureCurrent, "custom");
+                    mGestureCurrent = null;
+                }
+            }
+        });
+
+        updatePrefs();
+    }
+
+    private void updatePrefs() {
+        updateSummary(mGestureLeftUp, mGestureLeftUp.getValue());
+        updateSummary(mGestureMiddleUp, mGestureMiddleUp.getValue());
+        updateSummary(mGestureRightUp, mGestureRightUp.getValue());
+        updateSummary(mGestureLeftDown, mGestureLeftDown.getValue());
+        updateSummary(mGestureMiddleDown, mGestureMiddleDown.getValue());
+        updateSummary(mGestureRightDown, mGestureRightDown.getValue());
+        updateSummary(mGesturePinch, mGesturePinch.getValue());
+        updateSummary(mGestureSpread, mGestureSpread.getValue());
+        updateSummary(mGestureDoubleTap, mGestureDoubleTap.getValue());
+    }
+
+    private void updateSummary(ListPreference pref, String value) {
+        if (value.equals("custom")) {
+            pref.setSummary(AppHelper.getFriendlyNameForUri(
+                    getActivity(), getActivity().getPackageManager(),
+                    SettingsProvider.getString(getActivity(), pref.getKey() + "_custom", "")));
+        } else {
+            CharSequence[] array = pref.getEntryValues();
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].equals(value)) {
+                    pref.setSummary(pref.getEntries()[i]);
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGestureLeftUp || preference == mGestureMiddleUp ||
+                preference == mGestureRightUp || preference == mGestureLeftDown||
+                preference == mGestureMiddleDown || preference == mGestureRightDown ||
+                preference == mGesturePinch || preference == mGestureSpread ||
+                preference == mGestureDoubleTap) {
+            if (newValue.equals("custom")) {
+                mGesture = preference.getKey();
+                mGestureCurrent = (ListPreference) preference;
+                mPicker.pickShortcut(null, null, getId());
+                return true;
+            }
+            updateSummary((ListPreference) preference, (String) newValue);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != Activity.RESULT_CANCELED
+                && resultCode != Activity.RESULT_CANCELED) {
+            mPicker.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}

src/com/slim/slimlauncher/util/AppHelper.java
@@ -1,0 +1,137 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.slim.slimlauncher.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.util.Log;
+
+import com.slim.slimlauncher.R;
+
+import java.net.URISyntaxException;
+
+public class AppHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static String getProperSummary(Context context, PackageManager pm,
+                                          Resources settingsResources, String action, String values, String entries) {
+
+        if (pm == null || settingsResources == null || action == null) {
+            return context.getResources().getString(R.string.error_message_title);
+        }
+
+        if (values != null && entries != null) {
+            int resIdEntries = -1;
+            int resIdValues = -1;
+
+            resIdEntries = settingsResources.getIdentifier(
+                    SETTINGS_METADATA_NAME + ":array/" + entries, null, null);
+
+            resIdValues = settingsResources.getIdentifier(
+                    SETTINGS_METADATA_NAME + ":array/" + values, null, null);
+
+            if (resIdEntries > 0 && resIdValues > 0) {
+                try {
+                    String[] entriesArray = settingsResources.getStringArray(resIdEntries);
+                    String[] valuesArray = settingsResources.getStringArray(resIdValues);
+                    for (int i = 0; i < valuesArray.length; i++) {
+                        if (action.equals(valuesArray[i])) {
+                            return entriesArray[i];
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return getFriendlyNameForUri(context, pm, action);
+    }
+
+    public static String getFriendlyActivityName(Context context,
+                                                 PackageManager pm, Intent intent, boolean labelOnly) {
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+
+        if (friendlyName == null || friendlyName.startsWith("#Intent;")) {
+            return context.getResources().getString(R.string.error_message_title);
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    public static String getFriendlyShortcutName(
+            Context context, PackageManager pm, Intent intent) {
+        String activityName = getFriendlyActivityName(context, pm, intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName == null || activityName.startsWith("#Intent;")) {
+            return context.getResources().getString(R.string.error_message_title);
+        }
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public static String getFriendlyNameForUri(
+            Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return getFriendlyShortcutName(context, pm, intent);
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+    public static String getShortcutPreferred(
+            Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+            if (name == null || name.startsWith("#Intent;")) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return name;
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+}

Team-OctOS/platform_packages_apps_Launcher3

Conflicts:
	res/values/dimens.xml
Change-Id: I3418408c496a92261a9f30a3004653a1021d474d

src/com/android/launcher3/settings/GestureFragment.java
@@ -1,0 +1,136 @@
+package com.android.launcher3.settings;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+
+import com.android.launcher3.R;
+import com.android.launcher3.util.AppHelper;
+import com.android.launcher3.util.ShortcutPickHelper;
+
+public class GestureFragment extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+
+    ShortcutPickHelper mPicker;
+
+    ListPreference mGestureLeftUp;
+    ListPreference mGestureMiddleUp;
+    ListPreference mGestureRightUp;
+    ListPreference mGestureLeftDown;
+    ListPreference mGestureMiddleDown;
+    ListPreference mGestureRightDown;
+    ListPreference mGesturePinch;
+    ListPreference mGestureSpread;
+    ListPreference mGestureDoubleTap;
+
+    String mGesture;
+    ListPreference mGestureCurrent;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.gesture_fragment);
+
+        mGestureLeftUp = (ListPreference) findPreference(SettingsProvider.LEFT_UP_GESTURE_ACTION);
+        mGestureMiddleUp = (ListPreference)
+                findPreference(SettingsProvider.MIDDLE_UP_GESTURE_ACTION);
+        mGestureRightUp = (ListPreference) findPreference(SettingsProvider.RIGHT_UP_GESTURE_ACTION);
+        mGestureLeftDown = (ListPreference)
+                findPreference(SettingsProvider.LEFT_DOWN_GESTURE_ACTION);
+        mGestureMiddleDown = (ListPreference)
+                findPreference(SettingsProvider.MIDDLE_DOWN_GESTURE_ACTION);
+        mGestureRightDown = (ListPreference)
+                findPreference(SettingsProvider.RIGHT_DOWN_GESTURE_ACTION);
+        mGesturePinch = (ListPreference) findPreference(SettingsProvider.PINCH_GESTURE_ACTION);
+        mGestureSpread = (ListPreference) findPreference(SettingsProvider.SPREAD_GESTURE_ACTION);
+        mGestureDoubleTap = (ListPreference)
+                findPreference(SettingsProvider.DOUBLE_TAP_GESTURE_ACTION);
+
+        mGestureLeftUp.setOnPreferenceChangeListener(this);
+        mGestureMiddleUp.setOnPreferenceChangeListener(this);
+        mGestureRightUp.setOnPreferenceChangeListener(this);
+        mGestureLeftDown.setOnPreferenceChangeListener(this);
+        mGestureMiddleDown.setOnPreferenceChangeListener(this);
+        mGestureRightDown.setOnPreferenceChangeListener(this);
+        mGesturePinch.setOnPreferenceChangeListener(this);
+        mGestureSpread.setOnPreferenceChangeListener(this);
+        mGestureDoubleTap.setOnPreferenceChangeListener(this);
+
+        mPicker = new ShortcutPickHelper(getActivity(), new ShortcutPickHelper.OnPickListener() {
+            @Override
+            public void shortcutPicked(String uri, String friendlyName, boolean isApplication) {
+                if (uri == null) {
+                    return;
+                }
+                if (mGesture != null) {
+                    SettingsProvider.putString(mContext, mGesture + "_custom", uri);
+                    mGesture = null;
+                }
+                if (mGestureCurrent != null) {
+                    updateSummary(mGestureCurrent, "custom");
+                    mGestureCurrent = null;
+                }
+            }
+        });
+
+        updatePrefs();
+    }
+
+    private void updatePrefs() {
+        updateSummary(mGestureLeftUp, mGestureLeftUp.getValue());
+        updateSummary(mGestureMiddleUp, mGestureMiddleUp.getValue());
+        updateSummary(mGestureRightUp, mGestureRightUp.getValue());
+        updateSummary(mGestureLeftDown, mGestureLeftDown.getValue());
+        updateSummary(mGestureMiddleDown, mGestureMiddleDown.getValue());
+        updateSummary(mGestureRightDown, mGestureRightDown.getValue());
+        updateSummary(mGesturePinch, mGesturePinch.getValue());
+        updateSummary(mGestureSpread, mGestureSpread.getValue());
+        updateSummary(mGestureDoubleTap, mGestureDoubleTap.getValue());
+    }
+
+    private void updateSummary(ListPreference pref, String value) {
+        if (value.equals("custom")) {
+            pref.setSummary(AppHelper.getFriendlyNameForUri(
+                    getActivity(), getActivity().getPackageManager(),
+                    SettingsProvider.getString(getActivity(), pref.getKey() + "_custom", "")));
+        } else {
+            CharSequence[] array = pref.getEntryValues();
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].equals(value)) {
+                    pref.setSummary(pref.getEntries()[i]);
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGestureLeftUp || preference == mGestureMiddleUp ||
+                preference == mGestureRightUp || preference == mGestureLeftDown||
+                preference == mGestureMiddleDown || preference == mGestureRightDown ||
+                preference == mGesturePinch || preference == mGestureSpread ||
+                preference == mGestureDoubleTap) {
+            if (newValue.equals("custom")) {
+                mGesture = preference.getKey();
+                mGestureCurrent = (ListPreference) preference;
+                mPicker.pickShortcut(null, null, getId());
+                return true;
+            }
+            updateSummary((ListPreference) preference, (String) newValue);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode != Activity.RESULT_CANCELED
+                && resultCode != Activity.RESULT_CANCELED) {
+            mPicker.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+}

src/com/android/launcher3/util/AppHelper.java
@@ -1,0 +1,136 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.launcher3.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+
+import com.android.launcher3.R;
+
+import java.net.URISyntaxException;
+
+public class AppHelper {
+
+    private static final String SETTINGS_METADATA_NAME = "com.android.settings";
+
+    public static String getProperSummary(Context context, PackageManager pm,
+                                          Resources settingsResources, String action, String values, String entries) {
+
+        if (pm == null || settingsResources == null || action == null) {
+            return context.getResources().getString(R.string.error_message_title);
+        }
+
+        if (values != null && entries != null) {
+            int resIdEntries = -1;
+            int resIdValues = -1;
+
+            resIdEntries = settingsResources.getIdentifier(
+                    SETTINGS_METADATA_NAME + ":array/" + entries, null, null);
+
+            resIdValues = settingsResources.getIdentifier(
+                    SETTINGS_METADATA_NAME + ":array/" + values, null, null);
+
+            if (resIdEntries > 0 && resIdValues > 0) {
+                try {
+                    String[] entriesArray = settingsResources.getStringArray(resIdEntries);
+                    String[] valuesArray = settingsResources.getStringArray(resIdValues);
+                    for (int i = 0; i < valuesArray.length; i++) {
+                        if (action.equals(valuesArray[i])) {
+                            return entriesArray[i];
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return getFriendlyNameForUri(context, pm, action);
+    }
+
+    public static String getFriendlyActivityName(Context context,
+                                                 PackageManager pm, Intent intent, boolean labelOnly) {
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+
+        if (friendlyName == null || friendlyName.startsWith("#Intent;")) {
+            return context.getResources().getString(R.string.error_message_title);
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    public static String getFriendlyShortcutName(
+            Context context, PackageManager pm, Intent intent) {
+        String activityName = getFriendlyActivityName(context, pm, intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName == null || activityName.startsWith("#Intent;")) {
+            return context.getResources().getString(R.string.error_message_title);
+        }
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public static String getFriendlyNameForUri(
+            Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return getFriendlyShortcutName(context, pm, intent);
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+    public static String getShortcutPreferred(
+            Context context, PackageManager pm, String uri) {
+        if (uri == null || uri.startsWith("**")) {
+            return null;
+        }
+        try {
+            Intent intent = Intent.parseUri(uri, 0);
+            String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+            if (name == null || name.startsWith("#Intent;")) {
+                return getFriendlyActivityName(context, pm, intent, false);
+            }
+            return name;
+        } catch (URISyntaxException e) {
+        }
+
+        return uri;
+    }
+
+}