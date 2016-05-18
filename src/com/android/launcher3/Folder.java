SlimRoms/packages_apps_SlimLauncher

Add all folder options back
Change-Id: I17b35bfdbdec2adcea4fea174be3ccbec8bceba7

src/com/slim/slimlauncher/Folder.java
@@ -49,6 +49,7 @@ import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.slim.slimlauncher.FolderInfo.FolderListener;
+import com.slim.slimlauncher.settings.SettingsProvider;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -211,6 +212,13 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
         mFolderName.setInputType(mFolderName.getInputType() |
                 InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS | InputType.TYPE_TEXT_FLAG_CAP_WORDS);
         mAutoScrollHelper = new FolderAutoScrollHelper(mScrollView);
+
+        boolean hideFolderName = SettingsProvider.getBoolean(mLauncher,
+                SettingsProvider.HIDE_FOLDER_NAME, false);
+        if (hideFolderName) {
+            mFolderName.setVisibility(View.GONE);
+            mFolderNameHeight = 0;
+        }
     }
 
     private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
@@ -642,6 +650,10 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
         final BubbleTextView textView =
             (BubbleTextView) mInflater.inflate(R.layout.folder_application, this, false);
         textView.applyFromShortcutInfo(item, mIconCache, false);
+
+        int color = SettingsProvider.getInt(getContext(), SettingsProvider.FOLDER_ICON_TEXT_COLOR,
+                getResources().getColor(R.color.folder_items_text_color));
+        textView.setTextColor(color);
 
         textView.setOnClickListener(this);
         textView.setOnLongClickListener(this);

src/com/slim/slimlauncher/FolderIcon.java
@@ -24,9 +24,17 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Looper;
 import android.os.Parcelable;
 import android.util.AttributeSet;
@@ -43,6 +51,7 @@ import android.widget.TextView;
 
 import com.slim.slimlauncher.DropTarget.DragObject;
 import com.slim.slimlauncher.FolderInfo.FolderListener;
+import com.slim.slimlauncher.settings.SettingsProvider;
 
 import java.util.ArrayList;
 
@@ -163,6 +172,15 @@ public class FolderIcon extends FrameLayout implements FolderListener {
         lp.width = grid.folderIconSizePx;
         lp.height = grid.folderIconSizePx;
 
+        final int previewColor = SettingsProvider.getInt(launcher,
+                SettingsProvider.FOLDER_PREVIEW_COLOR, 0x71ffffff);
+        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+        DeviceProfile profile = LauncherAppState.getInstance().getDynamicGrid().getDeviceProfile();
+        drawable.setIntrinsicHeight(profile.iconSizePx);
+        drawable.setIntrinsicWidth(profile.iconSizePx);
+        drawable.getPaint().setColor(previewColor);
+        icon.mPreviewBackground.setImageDrawable(drawable);
+
         icon.setTag(folderInfo);
         icon.setOnClickListener(launcher);
         icon.mInfo = folderInfo;
@@ -171,6 +189,13 @@ public class FolderIcon extends FrameLayout implements FolderListener {
                 folderInfo.title));
         Folder folder = Folder.fromXml(launcher);
         folder.setDragController(launcher.getDragController());
+        int color = SettingsProvider.getInt(launcher,
+                SettingsProvider.FOLDER_BACKGROUND_COLOR, 0xffffffff);
+        ShapeDrawable sd = new ShapeDrawable(new RectShape());
+        sd.getPaint().setColor(color);
+        sd.getPaint().setStyle(Paint.Style.FILL_AND_STROKE);
+        sd.getPaint().setPathEffect(new CornerPathEffect(15));
+        folder.setBackground(sd);
         folder.setFolderIcon(icon);
         folder.bind(folderInfo);
         icon.mFolder = folder;

Team-OctOS/platform_packages_apps_Launcher3

Conflicts:
	res/values/colors.xml
Change-Id: I981b972b7c69e7cc9f67aeabb10ebc6d5f5f9b20

src/com/android/launcher3/Folder.java
@@ -57,6 +57,7 @@ import com.android.launcher3.FolderInfo.FolderListener;
 import com.android.launcher3.UninstallDropTarget.UninstallSource;
 import com.android.launcher3.Workspace.ItemOperator;
 import com.android.launcher3.accessibility.LauncherAccessibilityDelegate.AccessibilityDragSource;
+import com.android.launcher3.settings.SettingsProvider;
 import com.android.launcher3.util.Thunk;
 import com.android.launcher3.util.UiThreadCircularReveal;
 
@@ -214,6 +215,12 @@ public class Folder extends LinearLayout implements DragSource, View.OnClickList
         int measureSpec = MeasureSpec.UNSPECIFIED;
         mFooter.measure(measureSpec, measureSpec);
         mFooterHeight = mFooter.getMeasuredHeight();
+
+        boolean hideFolderName = SettingsProvider.getBoolean(mLauncher,
+                SettingsProvider.HIDE_FOLDER_NAME, false);
+        if (hideFolderName) {
+            mFolderName.setVisibility(View.GONE);
+        }
     }
 
     private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {

src/com/android/launcher3/FolderIcon.java
@@ -24,9 +24,17 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.CornerPathEffect;
+import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.RectShape;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.graphics.drawable.shapes.Shape;
 import android.os.Looper;
 import android.os.Parcelable;
 import android.util.AttributeSet;
@@ -43,6 +51,7 @@ import android.widget.TextView;
 
 import com.android.launcher3.DropTarget.DragObject;
 import com.android.launcher3.FolderInfo.FolderListener;
+import com.android.launcher3.settings.SettingsProvider;
 import com.android.launcher3.util.Thunk;
 
 import java.util.ArrayList;
@@ -167,6 +176,12 @@ public class FolderIcon extends FrameLayout implements FolderListener {
         lp.width = grid.folderIconSizePx;
         lp.height = grid.folderIconSizePx;
 
+        final int previewColor = SettingsProvider.getInt(launcher,
+                SettingsProvider.FOLDER_PREVIEW_COLOR, 0x71ffffff);
+        Drawable drawable = icon.mPreviewBackground.getDrawable().mutate();
+        drawable.setColorFilter(previewColor, PorterDuff.Mode.MULTIPLY);
+        icon.mPreviewBackground.setImageDrawable(drawable);
+
         icon.setTag(folderInfo);
         icon.setOnClickListener(launcher);
         icon.mInfo = folderInfo;
@@ -175,6 +190,11 @@ public class FolderIcon extends FrameLayout implements FolderListener {
                 folderInfo.title));
         Folder folder = Folder.fromXml(launcher);
         folder.setDragController(launcher.getDragController());
+        int color = SettingsProvider.getInt(launcher,
+                SettingsProvider.FOLDER_BACKGROUND_COLOR, 0xffffffff);
+        Drawable d = folder.getBackground().mutate();
+        d.setColorFilter(color, PorterDuff.Mode.MULTIPLY);
+        folder.setBackground(d);
         folder.setFolderIcon(icon);
         folder.bind(folderInfo);
         icon.mFolder = folder;